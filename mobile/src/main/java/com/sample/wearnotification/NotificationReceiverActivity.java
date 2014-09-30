cf15b9caf56deb42af0ff903c697d1cf2d6ef62f
Gabriel Bernardo Pereira <gabeira@gmail.com>
2014-09-29 22:17:28 -0400

    Create Wear Notification missing class

---
+++ mobile/src/main/java/com/sample/wearnotification/NotificationReceiverActivity.java
@@ -0,0 +1,15 @@
+package com.sample.wearnotification;
+
+/**
+ * Created by gabeira on 10/09/14.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationReceiverActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+    }
+}
