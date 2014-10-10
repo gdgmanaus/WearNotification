f5215d6db33f37276444d4ad5db24e938124f1d5
Gabriel Bernardo Pereira <gabeira@gmail.com>
2014-10-09 21:28:49 -0400

    Create Wear Notification missing class

--- mobile/src/main/java/com/sample/wearnotification/ActivityResult1.java
+++ mobile/src/main/java/com/sample/wearnotification/ActivityResult1.java
@@ -1,15 +1,16 @@
 package com.sample.wearnotification;
 
-/**
- * Created by gabeira on 10/09/14.
- */
 import android.app.Activity;
 import android.os.Bundle;
 
-public class NotificationReceiverActivity extends Activity {
+
+public class ActivityResult1 extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.result);
+        setContentView(R.layout.activity_activity_result1);
     }
+
+
 }
--- mobile/src/main/java/com/sample/wearnotification/MyActivity.java
+++ mobile/src/main/java/com/sample/wearnotification/MyActivity.java
@@ -2,15 +2,11 @@ package com.sample.wearnotification;
 
 import android.app.Activity;
 import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationCompat.WearableExtender;
 import android.support.v4.app.NotificationManagerCompat;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 
@@ -23,75 +19,41 @@ public class MyActivity extends Activity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void createNotification(View view) {
-        // Prepare intent which is triggered if the
-        // notification is selected
-        Intent intent = new Intent(this, NotificationReceiverActivity.class);
-        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-        // Build notification
-        // Actions are just fake
-        Notification noti = new NotificationCompat.Builder(this)
-                .setContentTitle("New mail from " + "test@gmail.com")
-                .setContentText("Subject")
-                .setSmallIcon(R.drawable.ic_launcher)
-                .setContentIntent(pIntent)
-                .addAction(R.drawable.ic_launcher, "Call", pIntent)
-                .addAction(R.drawable.ic_launcher, "More", pIntent)
-                .addAction(R.drawable.ic_launcher, "And more", pIntent).build();
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        // hide the notification after its selected
-        noti.flags |= NotificationCompat.FLAG_AUTO_CANCEL;
-
-        notificationManager.notify(0, noti);
-
-    }
-
     public void createWearNotification(View view){
 
         int notificationId = 001;
-        String eventDescription = "desc";
+//        String eventDescription = "Notificação";
         // Create an intent for the reply action
-        Intent actionIntent = new Intent(this, NotificationReceiverActivity.class);
+        Intent actionIntent = new Intent(this, ActivityResult1.class);
         PendingIntent actionPendingIntent =
                 PendingIntent.getActivity(this, 0, actionIntent,
                         PendingIntent.FLAG_UPDATE_CURRENT);
-
+        Intent actionIntent2 = new Intent(this, ActivityResult2.class);
+        PendingIntent actionPendingIntent2 =
+                PendingIntent.getActivity(this, 0, actionIntent2,
+                        0);
         // Create the action
-        NotificationCompat.Action action =
-                new NotificationCompat.Action.Builder(R.drawable.ic_launcher,getString(R.string.label), actionPendingIntent).build();
+        NotificationCompat.Action action1 =
+                new NotificationCompat.Action.Builder(R.drawable.ic_plusone_medium_off_client,getString(R.string.action1), actionPendingIntent).build();
+        NotificationCompat.Action action2 =
+                new NotificationCompat.Action.Builder(R.drawable.ic_launcher,getString(R.string.action2), actionPendingIntent2).build();
 
         // Specify the 'big view' content to display the long
         // event description that may not fit the normal content text.
         NotificationCompat.BigTextStyle bigStyle = new NotificationCompat.BigTextStyle();
-        bigStyle.bigText(eventDescription);
+        bigStyle.bigText(getString(R.string.notification));
 
         // Build the notification and add the action via WearableExtender
         Notification notification =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_launcher)
                         .setContentTitle(getString(R.string.title))
-                        .setContentText(getString(R.string.content))
-                        .extend(new WearableExtender().addAction(action))
+                         .setAutoCancel(true)
+                        .setOngoing(false)
+                        .setAutoCancel(true)
+                        .addAction(action1)
+                        .addAction(action2)
+                        //.extend(new NotificationCompat.WearableExtender().addAction(action2))
                         .setStyle(bigStyle)
                         .build();
 
